6de4b28366b93f2a8435d3590052689e33f9a242
william Montesino <williammontesino09jfr@gmail.com>
2026-04-05 13:27:50 -0400

    vamos a ver

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=1)
 DB_CONFIG = {
     'host': 'william23.mysql.pythonanywhere-services.com',
     'user': 'william23',
-    'password': '!KZ!U2AdpjA@ni@', # <--- CAMBIA ESTO POR TU CLAVE REAL
+    'password': 'TuNuevaClaveSinSimbolos', 
     'database': 'william23$bvc'
 }
 
